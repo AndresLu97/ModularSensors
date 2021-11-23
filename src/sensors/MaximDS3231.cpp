8d1dc4318474712a9e7e41e42c9e80ac740640c7
neilh20.gitk <neilh20+gitk1903@wLLw.net>
2021-11-23 15:57:42 -0800

    merge from enviroDIY (master) v0.32.2

--- src/sensors/MaximDS3231.cpp
+++ src/sensors/MaximDS3231.cpp
@@ -19,7 +19,7 @@ extern RTC_DS3231 rtc;  // was Sodaq_DS3231 rtc njh going to need revisiting
 MaximDS3231::MaximDS3231(uint8_t measurementsToAverage)
     : Sensor("MaximDS3231", DS3231_NUM_VARIABLES, DS3231_WARM_UP_TIME_MS,
              DS3231_STABILIZATION_TIME_MS, DS3231_MEASUREMENT_TIME_MS, -1, -1,
-             measurementsToAverage) {}
+             measurementsToAverage, DS3231_INC_CALC_VARIABLES) {}
 // Destructor
 MaximDS3231::~MaximDS3231() {}
 
