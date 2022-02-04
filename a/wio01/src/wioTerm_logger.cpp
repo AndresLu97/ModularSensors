dd5b40a27833aef15f294abc8835c00232529b53
neilh20 <neilh20+gitk1903@wLLw.net>
2022-02-03 18:18:03 -0800

    a/wio01 updates. Doesn't cc

--- a/wio01/src/wioTerm_logger.cpp
+++ a/wio01/src/wioTerm_logger.cpp
@@ -37,12 +37,13 @@ Notes:
 
 
 //https://www.forward.com.au/pfod/ArduinoProgramming/TimingDelaysInArduino.html
+#include <Arduino.h>
 #include <millisDelay.h>
 #include <Wire.h>
-#include <RTClib.h>
-//#include "RTC_SAMD51.h"
-#include <RTCZero.h>
-//#define RADIO_WIFI
+//#include <RTClib.h>
+#include "RTC_SAMD51.h"
+//#include <RTCZero.h>
+#define RADIO_WIFI
 #if defined RADIO_WIFI
 #include "ntpHelper.h"
 ntpHelper ntph;
@@ -59,19 +60,20 @@ uiHelper ui_display;
 const char ssid[] = "ArthurGuestSsid"; // add your required ssid
 const char password[] = "guest1234";//"your-passowrd"; // add your own netywork password
 #endif // RADIO_WIFI
+//RTC_DS3231 rtcPhy;
+RTC_SAMD51 rtcPhy; //.. in .h
+//RTCZero zero_sleep_rtc;
 
 millisDelay updateDelay; //ntp periodic update.
 
-DateTime now_dt; // time object
+DateTime now_dt, bootTime_dt ; // time object
 
 
 // localtime
 unsigned long devicetime;
 uint32_t readings_cnt =0;
 
-//RTC_DS3231 rtcPhy;
-//RTC_SAMD51 rtcPhy; .. in .h
-RTCZero zero_sleep_rtc;
+
 
 // for use by the Adafuit RTClib library
 //char daysOfTheWeek[7][12] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
@@ -109,6 +111,17 @@ void setup() {
     printFree();
     ui_display.begin();
     ui_display.fillscreen("Modular Sensors");
+
+    if (!rtcPhy.begin()) {
+        Serial.println("Couldn't find RTC");
+        //while (1) delay(10); // stop operating
+    }
+    // get and print the current rtc time
+    bootTime_dt = rtcPhy.now();
+    Serial.print("RTC boot time (UTC): ");
+    String bootTime(bootTime_dt.timestamp(DateTime::TIMESTAMP_FULL) );
+    Serial.println(bootTime);
+    //zero_sleep_rtc.begin();  rtcPhy instead
 #if defined RADIO_WIFI
     // setup network before rtc check 
     ntph.connectToWiFi(ssid, password);
@@ -117,32 +130,28 @@ void setup() {
     // getNTPtime returns epoch UTC time adjusted for timezone but not daylight savings
     // time
     devicetime = ntph.getNTPtime();
+    rtcPhy.adjust(devicetime);
+    now_dt= rtcPhy.now();
 #endif // RADIO_WIFI
-    zero_sleep_rtc.begin();
-    #if 0
-    if (!rtcPhy.begin()) {
-        Serial.println("Couldn't find RTC");
-        while (1) {
-            delay(5000); // stop operating
-            Serial.print(" Halted. No RTC!");
-        }
-    }
-    #endif //00
+
     DateTime ccTimeTZ(__DATE__, __TIME__);
     // check if rtc has lost power i.e. battery not present or flat or new device
-    //now_dt = rtcPhy.now();
-    now_dt= zero_sleep_rtc.getEpoch();
+    now_dt = rtcPhy.now();
+    //now_dt= bootTime_dt; //zero_sleep_rtc.getEpoch();
+
     if (! now_dt.isValid() ) {
         Serial.print("RTC lost power, set the time to ");
         // When time needs to be set on a new device, or after a power loss, 
         //DateTime ntp_dt(devicetime);
-        zero_sleep_rtc.setTime(ccTimeTZ.hour(), ccTimeTZ.minute(), ccTimeTZ.second());
-        zero_sleep_rtc.setDate(ccTimeTZ.date(), ccTimeTZ.month(), ccTimeTZ.year() - 2000);
-        now_dt = zero_sleep_rtc.getEpoch();
+        rtcPhy.adjust(ccTimeTZ);
+        //zero_sleep_rtc.setTime(ccTimeTZ.hour(), ccTimeTZ.minute(), ccTimeTZ.second());
+        //zero_sleep_rtc.setDate(ccTimeTZ.date(), ccTimeTZ.month(), ccTimeTZ.year() - 2000);
+        //now_dt = zero_sleep_rtc.getEpoch();
         //Serial.println(now_dt.timestamp(DateTime::TIMESTAMP_FULL));        
     }
     // get and print the current rtc time
     //now_dt = rtcPhy.now();
+    bootTime_dt = now_dt;
     Serial.print("RTC time is: ");
 
     Serial.println(now_dt.timestamp(DateTime::TIMESTAMP_FULL));
@@ -183,7 +192,7 @@ void loop() {
         // update rtc time
         devicetime = ntph.getNTPtime();
         #else
-        devicetime = zero_sleep_rtc.getEpoch();
+        devicetime = rtcPhy.now();
         #endif //RADIO_WIFI
         if (devicetime == 0) {
             Serial.println(" Failed to get time from network time server.");
@@ -194,7 +203,7 @@ void loop() {
 
             //Serial.print("rtc time updated.");
             // get and print the adjusted rtc time
-#if defined RADIO_WIFI
+#if 1 //defined RADIO_WIFI
             now_dt = rtcPhy.now();
 #else
             now_dt = zero_sleep_rtc.getEpoch();
